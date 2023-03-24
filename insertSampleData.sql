ff0b5de20efd0fc85410c6736ed12e2d5a350cbb
a1in9 <116525211+a1in9@users.noreply.github.com>
2023-03-24 14:40:07 -0400

    Game Insert
    move columns from temp to game

--- insertSampleData.sql
+++ insertSampleData.sql
@@ -1,7 +1,9 @@
 -- =============================== Game ===============================
 -- populate with video_games.csv (https://corgis-edu.github.io/corgis/csv/video_games/)
 -- we removed unnecessary columns for our project and renamed it as temp.csv (temp table imported into our DB)
-
+INSERT INTO Game (genre, title, release_date, unit_price)
+SELECT genres, title, CONCAT(year, '-01-01'), 'price'
+FROM temp;
 
 -- =============================== User ===============================
 
