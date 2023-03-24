d5b7e1e9efed628dd7f7a0767643aa2d9c743cae
a1in9 <116525211+a1in9@users.noreply.github.com>
2023-03-24 14:49:35 -0400

    Fix game inserts

--- insertSampleData.sql
+++ insertSampleData.sql
@@ -1,9 +1,11 @@
 -- =============================== Game ===============================
 -- populate with video_games.csv (https://corgis-edu.github.io/corgis/csv/video_games/)
--- we removed unnecessary columns for our project and renamed it as temp.csv (temp table imported into our DB)
+-- we removed unnecessary columns and renamed column headers for simplicity
+-- filw was renamed as temp.csv (temp table imported into our DB)
 INSERT INTO Game (genre, title, release_date, unit_price)
-SELECT genres, title, CONCAT(year, '-01-01'), 'price'
+SELECT genres, title, CONCAT(year, '-01-01'), price
 FROM temp;
+
 
 -- =============================== User ===============================
 
