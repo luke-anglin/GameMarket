8cc82369a52b2383cbda7cc7c2728a174e61d27a
Luke Anglin <lukeanglin@gmail.com>
2023-04-26 11:51:01 -0400

    issue with delete, need addition button too

--- update_game.php
+++ update_game.php
@@ -8,22 +8,22 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $delete = isset($_POST['delete']);
 
     // Prepare the SQL statement to update the game
-    $stmt = $db->prepare("UPDATE Game SET title = :title, unit_price = :unit_price WHERE game_id = :game_id");
-    $stmt->bindValue(':game_id', $game_id, PDO::PARAM_INT);
-    $stmt->bindValue(':title', $title, PDO::PARAM_STR);
-    $stmt->bindValue(':unit_price', $unit_price, PDO::PARAM_STR);
-
     if ($delete) {
         // If the delete checkbox is checked, delete the game instead
         $stmt = $db->prepare("DELETE FROM Game WHERE game_id = :game_id");
+        $stmt->bindValue(':game_id', $game_id);
+    } else {
+        $stmt = $db->prepare("UPDATE Game SET title = :title, unit_price = :unit_price WHERE game_id = :game_id");
         $stmt->bindValue(':game_id', $game_id, PDO::PARAM_INT);
+        $stmt->bindValue(':title', $title, PDO::PARAM_STR);
+        $stmt->bindValue(':unit_price', $unit_price, PDO::PARAM_STR);
     }
 
     // Execute the statement
     $stmt->execute();
 
     // Redirect back to the home page
-    header('Location: home.php');
+    // header('Location: home.php');
     exit();
 } else {
     // If the request method is not POST, redirect back to the home page
