8d7f42f181f9348d78c48f5e52af8637ef426885
shzher19991011 <128254945+shzher19991011@users.noreply.github.com>
2023-04-29 22:25:58 -0400

    Update stats-db.php

--- stats-db.php
+++ stats-db.php
@@ -78,4 +78,82 @@ function addPhone($phone, $id)
     $statement->closeCursor();
 }
 
+function getPurchases($id)
+{
+    global $db;
+    $current_uid = $_SESSION['user_id'];
+    $query = "SELECT purchase_date, game_id, seller_id FROM Purchases WHERE user_id = :id"; 
+    $statement = $db->prepare($query);
+    $statement->bindValue(':id', $id);
+    $statement->execute();
+    $results = $statement->fetchAll();
+    $statement->closeCursor();
+    return $results;
+}
+
+function addRating($rating, $u_id, $game_id)
+{
+    global $db;
+    $query = "SELECT COUNT(*) as count FROM Reviews WHERE user_id = :u_id AND game_id = :game_id"; 
+    $statement = $db->prepare($query);
+    $statement->bindValue(':u_id', $u_id);
+    $statement->bindValue(':game_id', $game_id);
+    $statement->execute();
+    $result = $statement->fetch();
+    $count = $result['count'];
+    $statement->closeCursor();
+
+    if ($count > 0) {
+        $query = "UPDATE Reviews SET rating = :rating WHERE user_id = :u_id AND game_id = :game_id"; 
+    } else {
+        $query = "INSERT INTO Reviews (user_id, game_id, rating) VALUES (:u_id, :game_id, :rating)"; 
+    }
+
+    $statement = $db->prepare($query);
+    $statement->bindValue(':u_id', $u_id);
+    $statement->bindValue(':game_id', $game_id);
+    $statement->bindValue(':rating', $rating);
+    $statement->execute();
+    $statement->closeCursor();
+}
+
+
+function getRating($id, $game_id)
+{
+    global $db;
+    $current_uid = $_SESSION['user_id'];
+    $query = "SELECT rating FROM Reviews WHERE user_id = :id AND game_id = :game_id"; 
+    $statement = $db->prepare($query);
+    $statement->bindValue(':id', $id);
+    $statement->bindValue(':game_id', $game_id);
+    $statement->execute();
+    $results = $statement->fetch();
+    $statement->closeCursor();
+    return $results;
+}
+
+function getUsername($seller_id)
+{
+    global $db;
+    $query = "SELECT username FROM User WHERE user_id=:seller_id"; 
+    $statement = $db->prepare($query);
+    $statement->bindValue(':seller_id', $seller_id);
+    $statement->execute();
+    $results = $statement->fetch();
+    $statement->closeCursor();
+    return $results;
+}
+
+function getGameTitle($game_id)
+{
+    global $db;
+    $query = "SELECT title FROM Game WHERE game_id=:game_id"; 
+    $statement = $db->prepare($query);
+    $statement->bindValue(':game_id', $game_id);
+    $statement->execute();
+    $results = $statement->fetch();
+    $statement->closeCursor();
+    return $results;
+}
+
 ?>
