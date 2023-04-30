bb8fea0fa4fad9d85d4199723818d6b7e1ab50b7
Luke Anglin <lukeanglin@gmail.com>
2023-04-30 18:38:53 -0400

    check

--- update_game.php
+++ update_game.php
@@ -30,21 +30,45 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 ?>
-
-<form action="update_game.php" method="POST">
+<!-- Include necessary Bootstrap CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+  integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+  crossorigin="anonymous">
+<!-- Start form with Bootstrap classes -->
+<form class="form-horizontal" action="update_game.php" method="POST">
   <div class="form-group">
-    <label for="title">Title</label>
-    <input type="text" class="form-control" id="title" name="title" value="<?php echo $title; ?>">
+    <label for="title" class="col-sm-2 control-label">Title</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="title" name="title" value="<?php echo $title; ?>">
+    </div>
   </div>
   <div class="form-group">
-    <label for="unit_price">Unit Price</label>
-    <input type="number" class="form-control" id="unit_price" name="unit_price" value="<?php echo $unit_price; ?>">
+    <label for="unit_price" class="col-sm-2 control-label">Unit Price</label>
+    <div class="col-sm-10">
+      <input type="number" class="form-control" id="unit_price" name="unit_price" value="<?php echo $unit_price; ?>">
+    </div>
   </div>
   <div class="form-group">
-    <label for="genre">Genre</label>
-    <input type="text" class="form-control" id="genre" name="genre" value="<?php echo $genre; ?>">
+    <label for="genre" class="col-sm-2 control-label">Genre</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="genre" name="genre" value="<?php echo $genre; ?>">
+    </div>
   </div>
   <input type="hidden" name="game_id" value="<?php echo $game_id; ?>">
   <input type="hidden" name="initial" value="0">
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </div>
+  </div>
 </form>
+<!-- Include necessary Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+  integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+  crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+  integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+  crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+  integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+  crossorigin="anonymous"></script>
